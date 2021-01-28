a7000978a23a8188eece168181e292d356ef129f
Evert Ramos <evert.ramos@cgu.gov.br>
2021-01-27 21:59:44 -0300

    backup check tar file and decompress

---
+++ backup/backup-check-tar-file-integrity.sh
@@ -0,0 +1,47 @@
+#-----------------------------------------------------------------------
+#
+# Basescript functions
+#
+# The basescript functions were designed to work as abstract function,
+# so it could be used in many different contexts executing specific job
+# always remembering Unix concept DOTADIW - "Do One Thing And Do It Well"
+#
+# Developed by
+#   Evert Ramos <evert.ramos@gmail.com>
+#
+# Copyright Evert Ramos
+#
+#-----------------------------------------------------------------------
+#
+# Be carefull when editing this file, it is part of a bigger script!
+#
+# Basescript - https://github.com/evertramos/basescript
+#
+#-----------------------------------------------------------------------
+
+#-----------------------------------------------------------------------
+# This function has one main objective:
+# 1. Check the integrity of the informed tar file
+#
+# You must/might inform the parameters below:
+# 1. The file name including the full path
+# 2. [optional] (default: ) n/a
+#
+#-----------------------------------------------------------------------
+
+backup_check_tar_file_integrity()
+{
+    local LOCAL_FULLFILE_NAME LOCAL_RESULT
+     
+    LOCAL_FULLFILE_NAME=${1:-null}
+ 
+    [[ $LOCAL_FULLFILE_NAME == "" || $LOCAL_FULLFILE_NAME == null ]] && echoerr "You must inform the required argument(s) to the function: \n'${FUNCNAME[0]}' \nplease check the docs."
+ 
+    [[ "$DEBUG" == true ]] && echo "Checking file's integrity: '$LOCAL_FULLFILE_NAME'"
+    
+    LOCAL_RESULT=$(tar tf $LOCAL_FULLFILE_NAME &> /dev/null || echo 1)
+
+    [[ $LOCAL_RESULT == 1 ]] && echoerr "The file informed is currupted or was not found: \n'$LOCAL_FULLFILE_NAME' \nplease check and try again."
+
+    return 0 
+}
---
+++ backup/backup-decompress-file.sh
@@ -0,0 +1,66 @@
+#-----------------------------------------------------------------------
+#
+# Basescript functions
+#
+# The basescript functions were designed to work as abstract function,
+# so it could be used in many different contexts executing specific job
+# always remembering Unix concept DOTADIW - "Do One Thing And Do It Well"
+#
+# Developed by
+#   Evert Ramos <evert.ramos@gmail.com>
+#
+# Copyright Evert Ramos
+#
+#-----------------------------------------------------------------------
+#
+# Be carefull when editing this file, it is part of a bigger script!
+#
+# Basescript - https://github.com/evertramos/basescript
+#
+#-----------------------------------------------------------------------
+
+#-----------------------------------------------------------------------
+# This function has one main objective:
+# 1. Decompress a backup file
+#
+# You must/might inform the parameters below:
+# 1. Destination folder where the file will be decompresses
+# 2. Compressed file name including the full path
+# 3. [optional] (default: true) Run as sudo
+#
+#-----------------------------------------------------------------------
+
+backup_decompress_file()
+{
+
+    local LOCAL_DESTINATION_FOLDER LOCAL_BACKUP_FULL_FILE 
+    
+    LOCAL_DESTINATION_FOLDER=${1:-null}
+    LOCAL_BACKUP_FULL_FILE=${2:-null}
+    RUN_WITH_SUDO=${3:-true}
+
+    [[ $LOCAL_DESTINATION_FOLDER == "" || $LOCAL_DESTINATION_FOLDER == null ]] || [[ $LOCAL_BACKUP_FULL_FILE == "" || $LOCAL_BACKUP_FULL_FILE == null ]] && echoerr "You must inform DESTINATION and FULLFILE NAME to the function: \n'${FUNCNAME[0]}' \nplease check the docs."
+
+    run_function system_check_package_installed "tar" true
+    run_function system_check_package_installed "gzip" true
+    
+    [[ "$DEBUG" == true ]] && echo "Decompressing file '$LOCAL_BACKUP_FULL_FILE' to '$LOCAL_DESTINATION_FOLDER'."
+
+    cd $LOCAL_DESTINATION_FOLDER 
+
+    if [[ "$DEBUG" != true ]]; then
+        if [[ "$RUN_WITH_SUDO" == true ]]; then
+            sudo tar -xzf $LOCAL_BACKUP_FULL_FILE
+        else
+            tar -xzf $LOCAL_BACKUP_FULL_FILE
+        fi
+    else
+        if [[ "$RUN_WITH_SUDO" == true ]]; then
+            sudo tar --verbose -xzf $LOCAL_BACKUP_FULL_FILE
+        else
+            tar --verbose -xzf $LOCAL_BACKUP_FULL_FILE
+        fi
+    fi
+    cd - > /dev/null 2>&1
+}
+
