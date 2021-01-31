d05c900d9be767a89436cb665fb46a32598e5ae0
Evert Ramos <evert.ramos@cgu.gov.br>
2021-01-31 19:28:57 -0300

    System check package installation dpkg

--- docs/README.md
+++ docs/README.md
@@ -2,6 +2,16 @@
 
 The list below is in alphabetical order.
 
+# Requirements
+
+We choose to use some packages which is mostly common in many linux distros, 
+sorry if we choose something you would rather not to use or do not have in your distro.
+Please consider update the function to embrace you and the community.
+
+- sudo
+- dkpg
+- apt-install (roadmap to check ditro and use *yum*)
+
 ## Usability
 
 @todo
@@ -19,3 +29,26 @@ The list below is in alphabetical order.
 ### function 1
 
 ### function 2
+
+
+
+## System functions
+
+### Check if package is installed
+
+name: *system_check_package_installed*
+
+requirement(s): *dpkg*, [*apt-install*, *sudo*] 
+
+This function has one main objective to check if the informed package is already installed in the system. 
+It has an extra option to install the missing package. 
+In order to install, the user must have *sudo* configured, *apt-install* installed and *dpkg* package manager.
+
+The install uses the option as below:
+
+```bash
+$ apt-get install -y --no-install-recommends $PACKAGE_NAME
+```
+
+> If you use other package manager besides *apt-get* you might install manually the required packages once the defatult
+> option in this function is to skip the installation process, so you can still use this function in other scripts.
---
+++ system/system-check-package-installed.sh
@@ -0,0 +1,59 @@
+#-----------------------------------------------------------------------
+#
+# Basescript function
+#
+# The basescript functions were designed to work as abstract function,
+# so it could be used in many different contexts executing specific job
+# always remembering Unix concept DOTADIW - "Do One Thing And Do It Well"
+#
+# Developed by
+#   Evert Ramos <evert.ramos@gmail.com>
+#
+# Copyright Evert Ramos
+#
+#-----------------------------------------------------------------------
+#
+# Be carefull when editing this file, it is part of a bigger script!
+#
+# Basescript - https://github.com/evertramos/basescript
+#
+#-----------------------------------------------------------------------
+
+#-----------------------------------------------------------------------
+# This function has one main objective:
+# 1. Check if a package is installed and install it if it is not
+#
+# You must/might inform the parameters below:
+# 1. Package name
+# 2. [optional] (default: false) Install package
+#
+#-----------------------------------------------------------------------
+
+system_check_package_installed()
+{
+    local LOCAL_PACKAGE LOCAL_INSTALL_PACKAGED LOCAL_RESULT
+     
+    LOCAL_PACKAGE=${1:-null}
+    LOCAL_INSTALL_PACKAGED=${2:-false}
+
+    [[ $LOCAL_PACKAGE == "" ]] && echoerr "You must inform the PACKAGE NAME to the function: '${FUNCNAME[0]}'"
+ 
+    [[ "$DEBUG" == true ]] && echo "Checking if package '$LOCAL_PACKAGE' is installed."
+
+    LOCAL_RESULT=$(dpkg -s $LOCAL_PACKAGE >/dev/null 2>&1 || echo 1)
+
+    if [[ "$LOCAL_INSTALL_PACKAGED" == true ]] && [[ $LOCAL_RESULT == 1 ]]; then
+        [[ "$DEBUG" == true ]] && echo "Installing package '$LOCAL_PACKAGE'."
+
+        sudo apt-get install -y --no-install-recommends $LOCAL_PACKAGE || echoerr "There were errors when installing package '$LOCAL_PACKAGE'."
+        
+        LOCAL_RESULT=$(dpkg -s tars >/dev/null 2>&1)
+    fi
+    
+    if [[ $LOCAL_RESULT != '' ]]; then
+        PACKAGE_IS_INSTALLED=true 
+    else
+        PACKAGE_IS_INSTALLED=false
+    fi
+}
+
