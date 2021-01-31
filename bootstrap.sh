63a55ca9737adf48ef5f9c652c4452037b49a60b
Evert Ramos <evert.ramos@cgu.gov.br>
2021-01-31 19:05:53 -0300

    Update bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,10 @@
 #-----------------------------------------------------------------------
 #
-# Bootstrap basescript 
+# Basescript function
 #
-# Loads all functions in ./basescript folder
+# The basescript functions were designed to work as abstract function,
+# so it could be used in many different contexts executing specific job
+# always remembering Unix concept DOTADIW - "Do One Thing And Do It Well"
 #
 # Developed by
 #   Evert Ramos <evert.ramos@gmail.com>
@@ -11,16 +13,21 @@
 #
 #-----------------------------------------------------------------------
 #
-# Be carefull when editing this file, it is part of a bigger scripts!
+# Be carefull when editing this file, it is part of a bigger script!
 #
 # Basescript - https://github.com/evertramos/basescript
 #
 #-----------------------------------------------------------------------
 
 #-----------------------------------------------------------------------
-# Fill out local variables 
+# This script has one main objective:
+# 1. Load all functions in ./basescript folder
+#
 #-----------------------------------------------------------------------
 
+#-----------------------------------------------------------------------
+# Fill out local variables 
+#-----------------------------------------------------------------------
 # Get Current directory
 LOCAL_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd)"
 
@@ -33,25 +40,23 @@ BOOTSTRAP_FILE_NAME="bootstrap.sh"
 [[ "$DEBUG" == true ]] && "Reading base script files... [bootstrap.sh]"
 
 #-----------------------------------------------------------------------
-# Start reading files
+# Read files with extension '.sh'
 #-----------------------------------------------------------------------
-
 # Loop the base folder and source all files in root folder
 for file in $LOCAL_PATH/*.sh
 do
-  [[ $file != $LOCAL_PATH/$BOOTSTRAP_FILE_NAME ]] && source $file
+    [[ $file != $LOCAL_PATH/$BOOTSTRAP_FILE_NAME ]] && source $file
 done
 
 # Loop through all folders in the base folder and source all files inside
 for dir in $LOCAL_PATH/*/
 do
-  local_dir=${dir%*/}
-    
-  for file in $local_dir/*.sh
-  do
-    [[ $file != $LOCAL_PATH/$BOOTSTRAP_FILE_NAME ]] && source $file
-  done
+    local_dir=${dir%*/}
+
+    for file in $local_dir/*.sh
+    do
+        [[ $file != $LOCAL_PATH/$BOOTSTRAP_FILE_NAME ]] && source $file
+    done
 done
 
 return 0
-
