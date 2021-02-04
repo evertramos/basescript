5b231b732e5596aae86934036c7f828a7fa0984b
Evert Ramos <evert.ramos@cgu.gov.br>
2021-02-04 15:31:04 -0300

    Check in the dns service provider if a domain exists

---
+++ domain/check-domain-exists.sh
@@ -0,0 +1,68 @@
+#-----------------------------------------------------------------------
+#
+# Basescript function
+#
+# The basescript functions were designed to work as abstract function,
+# so it could be used in many different contexts executing specific job
+# always remembering Unix concept DOTADIW - "Do One Thing And Do It Well"
+#
+# Developed by
+#   Evert Ramos <evert.ramos@gmail.com>
+#
+# Copyright Evert Ramos
+#
+#-----------------------------------------------------------------------
+#
+# Be carefull when editing this file, it is part of a bigger script!
+#
+# Basescript - https://github.com/evertramos/basescript
+#
+#-----------------------------------------------------------------------
+
+#-----------------------------------------------------------------------
+# This function has one main objective:
+# 1. Check if the informed domain exists in your DNS service
+#
+# You must/might inform the parameters below:
+# 1. Domain name
+# 2. [optional] (default: 'digitalocean') The DNS Service you use
+#
+# Available DNS Service Providers:
+#   - Digital Ocean
+#
+#-----------------------------------------------------------------------
+
+check_domain_exists()
+{
+    local LOCAL_DOMAIN LOCAL_PROVIDER
+    
+    LOCAL_DOMAIN=${1:-$DOMAIN}
+    LOCAL_PROVIDER=${2:-'digitalocean'}
+
+    [[ $LOCAL_DOMAIN == "" || $LOCAL_DOMAIN == null ]] && echoerr "You must inform the domain name to the function: '${FUNCNAME[0]}'"
+
+    [[ "$DEBUG" == true ]] && echo "Checking if domain '"$LOCAL_DOMAIN"' exists"
+
+    if [[ $LOCAL_PROVIDER == "digitalocean" ]]; then
+        # Digital Ocean ready
+        # @todo improve for an abstract function - add CloudFlare and AWS
+        RESPONSE="$(curl -X GET -H "Authorization: Bearer $API_KEY" -H "Content-Type: application/json" \
+            "https://api.digitalocean.com/v2/domains/$LOCAL_DOMAIN" | jq 'select(.domain != null) | .domain.name')"
+    else
+      echoerr "The service provider '$LOCAL_PROVIDER' is not supported by this function [${FUNCNAME[0]}]"
+    fi
+
+    [[ "$DEBUG" == true ]] && echo "RESPONSE: "${RESPONSE:-Not found!}
+
+    if [[ ! -z "${RESPONSE}" ]]; then
+        DOMAIN_EXIST=true
+        return 0
+    else
+        DOMAIN_EXIST=false
+        # @todo improvemnt
+        # If domain doesn´t exist create it if error on create then show message
+        #MESSAGE="This domain $DOMAIN does not exist in your account."
+        #return 1
+        return 0
+    fi
+}
