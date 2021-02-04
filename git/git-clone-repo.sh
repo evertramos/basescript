da8cd9ad52ab929ceb34b87832d5d8771eb1b342
Evert Ramos <evert.ramos@cgu.gov.br>
2021-02-04 16:36:17 -0300

    Git cloning repository

---
+++ git/git-clone-repo.sh
@@ -0,0 +1,73 @@
+#-----------------------------------------------------------------------
+#
+# Basescript function
+#
+# The basescript functions were designed to work as abstract function,
+# so it could be used in many different contexts executing specific job
+# always remembering Unix concept DOTADIW - "Do One Thing And Do It Well"
+#
+# Developed by
+#   Evert Ramos <evert.ramos@gmail.com>
+#
+# Copyright Evert Ramos
+#
+#-----------------------------------------------------------------------
+#
+# Be carefull when editing this file, it is part of a bigger script!
+#
+# Basescript - https://github.com/evertramos/basescript
+#
+#-----------------------------------------------------------------------
+
+#-----------------------------------------------------------------------
+# This function has one main objective:
+# 1. Clone a git repo
+#
+# You must/might inform the parameters below:
+# 1. git repo
+# 2. destination path (fullpath)
+# 3. [optional] (default: master) branch/tag name
+# 4. [optional] (default: null) destination folder if y
+# 5. [optional] (default: false) clone full repo with all branches
+#
+#-----------------------------------------------------------------------
+
+#-----------------------------------------------------------------------
+# This function were designed for automation process on docker services
+# having the default option '--depth 1' when cloning a git repository
+# set 'true' in the argument above to clone all branches instead
+#-----------------------------------------------------------------------
+
+git_clone_repo()
+{
+    local LOCAL_GIT_REPO LOCAL_FULL_PATH LOCAL_REPO_BRANCH LOCAL_GIT_FOLDER_NAME LOCAL_FULL_REPO
+     
+    LOCAL_GIT_REPO=${1:-null}
+    LOCAL_FULL_PATH=${2:-null}
+    LOCAL_REPO_BRANCH=${3:-"master"}
+    LOCAL_GIT_FOLDER_NAME=${4}
+    LOCAL_FULL_REPO=${5:-false}
+ 
+    [[ $LOCAL_FULL_PATH == "" || $LOCAL_FULL_PATH == null ]] && echoerr "You must inform the 'git repo' and the 'local path' \n to the function: '${FUNCNAME[0]}'"
+ 
+    # Prepare the FULL_PATH with LOCAL_GIT_FOLDER_NAME
+    [[ $LOCAL_GIT_FOLDER_NAME != "" ]] && LOCAL_FULL_PATH=${LOCAL_FULL_PATH%/}"/"$LOCAL_GIT_FOLDER_NAME
+
+    [[ "$DEBUG" == true ]] && echo "Cloning repo '$LOCAL_GIT_REPO' branch '$LOCAL_REPO_BRANCH' to '$LOCAL_FULL_PATH'."
+
+    LOCAL_GIT_REPO_ONLINE=$(git ls-remote --exit-code --quiet ${LOCAL_GIT_REPO} --tags ${LOCAL_REPO_BRANCH})
+    LOCAL_GIT_REPO_ONLINE=$?
+    if [[ $LOCAL_GIT_REPO_ONLINE != 0 ]]; then
+        RESPONSE_GIT_CLONE_REPO="Git repo $LOCAL_GIT_REPO with tag ${LOCAL_REPO_BRANCH} is not reachable."
+        return 0
+    fi
+
+    mkdir -p $LOCAL_FULL_PATH
+    if [[ "$LOCAL_FULL_REPO" == true ]]; then
+        git clone --branch $LOCAL_REPO_BRANCH $LOCAL_GIT_REPO $LOCAL_FULL_PATH > /dev/null 2>&1
+    else
+        git clone --depth 1 --branch $LOCAL_REPO_BRANCH $LOCAL_GIT_REPO $LOCAL_FULL_PATH > /dev/null 2>&1
+    fi
+
+    return 0
+}
