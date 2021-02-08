d974744d92110366b26ec40aee51668079a730ee
Evert Ramos <evert.ramos@cgu.gov.br>
2021-02-08 17:05:29 -0300

    Delete local file restrict to specified folder

---
+++ file/file-delete-local-file.sh
@@ -0,0 +1,45 @@
+#-----------------------------------------------------------------------
+#
+# Basescript function
+#
+# The basescript functions were designed to work as abstract function,
+# so it could be used in many different contexts executing specific job
+# always remembering Unix concept DOTADIW - "Do One Thing And Do It Well"
+#
+# Developed by
+#   Evert Ramos <evert.ramos@gmail.com>
+#
+# Copyright Evert Ramos
+#
+#-----------------------------------------------------------------------
+#
+# Be carefull when editing this file, it is part of a bigger script!
+#
+# Basescript - https://github.com/evertramos/basescript
+#
+#-----------------------------------------------------------------------
+
+#-----------------------------------------------------------------------
+# This function has one main objective:
+# 1. Delete a specific file
+#
+# You must/might inform the parameters below:
+# 1. File to be deleted
+# 2. [optional] (default: ) n/a
+#
+#-----------------------------------------------------------------------
+
+file_delete_local_file()
+{
+    local LOCAL_FILE
+     
+    LOCAL_FILE=${1:-null}
+
+    [[ $LOCAL_FILE == "" ]] && echoerr "You must inform the required argument(s) to the function: \n'${FUNCNAME[0]}' \nplease check the docs."
+ 
+    [[ "$DEBUG" == true ]] && echo "Deleting file '$LOCAL_FILE'"
+
+    rm $LOCAL_FILE
+
+    return 0
+}
