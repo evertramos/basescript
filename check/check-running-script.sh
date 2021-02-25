d1f6b5706d59f530d16b523d37357d4a8d8e496b
Evert Ramos <evert.ramos@cgu.gov.br>
2021-02-25 06:29:39 -0300

    Add check runnint script

---
+++ check/check-running-script.sh
@@ -0,0 +1,45 @@
+#-----------------------------------------------------------------------
+#
+# Basescript function
+#
+# The basescript functions were designed to work as abstract function,
+# so it could be used in many different contexts executing specific job
+# always remembering Unix concept DOTADIW - "Do One Thing And Do It Well"
+#
+# Developed by
+#   Evert Ramos <evert.ramos@gmail.com>
+#
+# Copyright Evert Ramos
+#
+#-----------------------------------------------------------------------
+#
+# Be carefull when editing this file, it is part of a bigger script!
+#
+# Basescript - https://github.com/evertramos/basescript
+#
+#-----------------------------------------------------------------------
+
+#-----------------------------------------------------------------------
+# This function has one main objective:
+# 1. Check if there is another instance of the script running
+#
+# You must/might inform the parameters below:
+# 1. Pid file name
+# 2. [optional] (default: ) n/a
+#
+#-----------------------------------------------------------------------
+
+check_running_script()
+{
+    local LOCAL_PID_FILE
+    
+    LOCAL_PID_FILE=${1:-$PID_FILE}
+    PID=$SCRIPT_PATH/$LOCAL_PID_FILE
+
+    [[ "$DEBUG" == true ]] && echo "Checking if there is another instance of the script running..." && echo "pid: "$PID
+
+    if [[ -e "$PID" ]]; then
+        MESSAGE="Script already running."
+        return 1
+    fi
+}
