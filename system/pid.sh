7882674e40839ca7b636d29e6957b15f51e14370
Evert Ramos <evert.ramos@cgu.gov.br>
2021-02-25 19:46:33 -0300

    Add system pid function

---
+++ system/pid.sh
@@ -0,0 +1,47 @@
+#-----------------------------------------------------------------------
+#
+# Basescript function
+#
+# The basescript functions were designed to work as abstract function,
+# so it could be used in many different contexts executing specific job
+# always remembering Unix concept DOTADIW - "Do One Thing And Do It Well"
+#
+# Developed by
+#   Evert Ramos <evert.ramos@gmail.com>
+#
+# Copyright Evert Ramos
+#
+#-----------------------------------------------------------------------
+#
+# Be careful when editing this file, it is part of a bigger script!
+#
+# Basescript - https://github.com/evertramos/basescript
+#
+#-----------------------------------------------------------------------
+
+#-----------------------------------------------------------------------
+# This function has one main objective:
+# 1. Save and delete pid file
+#
+# You must/might inform the parameters below:
+# 1. Description of the file name that will be created with the
+# pid number
+# 2. [optional] (default: )
+#
+#-----------------------------------------------------------------------
+
+# Script to work with PID
+system_save_pid() {
+    local LOCAL_PID_FILE
+    LOCAL_PID_FILE=${1:-$PID_FILE}
+    [[ $LOCAL_PID_FILE== ""  || $LOCAL_PID_FILE== null ]] && echoerr "You must inform the pid file name to the function: '${FUNCNAME[0]}'"
+    echo $$ > $SCRIPT_PATH/$LOCAL_PID_FILE
+    trap "system_delete_pid $LOCAL_PID_FILE" EXIT SIGQUIT SIGINT SIGSTOP SIGTERM ERR
+}
+
+system_delete_pid() {
+    local LOCAL_PID_FILE
+    LOCAL_PID_FILE=${1:-$PID_FILE}
+    [[ $LOCAL_PID_FILE== ""  || $LOCAL_PID_FILE== null ]] && echoerr "You must inform the pid file name to the function: '${FUNCNAME[0]}'"
+    rm -f $SCRIPT_PATH/$LOCAL_PID_FILE
+}
