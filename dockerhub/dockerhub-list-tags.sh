da9a6f299d5932278619a8384d284de06fdfff6e
Evert Ramos <evert.ramos@cgu.gov.br>
2021-03-03 21:36:53 -0300

    Add option to use different string comparison

--- dockerhub/dockerhub-list-tags.sh
+++ dockerhub/dockerhub-list-tags.sh
@@ -25,20 +25,25 @@
 #
 # You must/might inform the parameters below:
 # 1. Image name
-# 2. [optional] (default: ) n/a
+# 2. [optional] (default: true) Get versions with two digits
 #
 #-----------------------------------------------------------------------
 
 dockerhub_list_tags()
 {
-    local LOCAL_IMAGE
+    local LOCAL_IMAGE LOCAL_GET_TWO_DIGITS_VERSIONS
      
     LOCAL_IMAGE=${1:-null}
+    LOCAL_GET_TWO_DIGITS_VERSIONS=${2:-true}
 
     [[ $LOCAL_IMAGE == "" || $LOCAL_IMAGE == null ]] && echoerr "You must inform at least the image name to the function: '${FUNCNAME[0]}'"
 
     [[ "$DEBUG" == true ]] && echo "Listing tags from docker hub for image '$LOCAL_IMAGE'"
 
-    # This command will filter the number versions and limited to one dot (.)
-    DOCKERHUB_LIST_TAGS=($(wget -q https://registry.hub.docker.com/v1/repositories/$LOCAL_IMAGE/tags -O - | sed -e 's/[][]//g' -e 's/"//g' -e 's/ //g' | tr '}' '\n'  | awk -F: '{print $3}' | grep '^[0-9]' | grep -v '-' | grep -v '\.[0-9]\.'))
+    # This command will filter the number versions and limited to one dot (.) and two versions such as 0.1 or all versions if LOCAL_GET_TWO_DIGITS_VERSIONS is set to false
+    if [[ "$LOCAL_GET_TWO_DIGITS_VERSIONS" == true ]]; then
+        DOCKERHUB_LIST_TAGS=($(wget -q https://registry.hub.docker.com/v1/repositories/$LOCAL_IMAGE/tags -O - | sed -e 's/[][]//g' -e 's/"//g' -e 's/ //g' | tr '}' '\n'  | awk -F: '{print $3}' | grep '^[0-9]' | grep -v '-' | grep -v '\.[0-9]\.'))
+    else
+        DOCKERHUB_LIST_TAGS=($(wget -q https://registry.hub.docker.com/v1/repositories/$LOCAL_IMAGE/tags -O - | sed -e 's/[][]//g' -e 's/"//g' -e 's/ //g' | tr '}' '\n'  | awk -F: '{print $3}' | grep '^[0-9]' | grep -v '-'))
+    fi
 }
