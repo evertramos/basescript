3a51e3e5a02ab350601b3d546e87b4a413ee5344
Evert Ramos <evert.ramos@cgu.gov.br>
2021-03-08 18:01:52 -0300

    Add email validation check

---
+++ email/email_check_is_valid.sh
@@ -0,0 +1,46 @@
+#-----------------------------------------------------------------------
+#
+# Basescript function
+#
+# The basescript functions were designed to work as abstract function,
+# so it could be used in many different contexts executing specific job
+# always remembering Unix concept DOTADIW - "Do One Thing And Do It Well"
+#
+# Developed by
+#   Evert Ramos <evert.ramos@gmail.com>
+#
+# Copyright Evert Ramos
+#
+#-----------------------------------------------------------------------
+#
+# Be careful when editing this file, it is part of a bigger script!
+#
+# Basescript - https://github.com/evertramos/basescript
+#
+#-----------------------------------------------------------------------
+
+#-----------------------------------------------------------------------
+# This function has one main objective:
+# 1. Check if there is a valid email address
+#
+# You must/might inform the parameters below:
+# 1. Email address that should be checked
+# 2. [optional] (default: ) n/a
+#
+#-----------------------------------------------------------------------
+
+email_check_is_valid()
+{
+    local LOCAL_EMAIL_ADDRESS
+
+    LOCAL_EMAIL_ADDRESS=${1:-null}
+
+    # Check required
+    [[ $LOCAL_EMAIL_ADDRESS == "" || $LOCAL_EMAIL_ADDRESS == null ]] && echoerr "You must inform the required argument(s) to the function: '${FUNCNAME[0]}'"
+
+    if [[ $LOCAL_EMAIL_ADDRESS =~ ^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$ ]]; then
+        EMAIL_IS_VALID=true
+    else
+        EMAIL_IS_VALID=false
+    fi
+}
