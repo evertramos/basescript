4bcdcbf21384f3cb4b2f69477bc9642d3e7840ef
Evert Ramos <evert.ramos@cgu.gov.br>
2021-03-08 13:50:20 -0300

    IP check and get

---
+++ ip/ip_check_ipv4.sh
@@ -0,0 +1,46 @@
+#-----------------------------------------------------------------------
+#
+# Basescript function
+#
+# The basescript functions were designed to work as abstract function,
+# so it could be used in many different contexts executing specific job
+# always remembering Unix concept DOTADIW - "Do One Thing And Do It Well"
+#
+# Developed by
+#   Evert Ramos <evert.ramos@gmail.com>
+#
+# Copyright Evert Ramos
+#
+#-----------------------------------------------------------------------
+#
+# Be careful when editing this file, it is part of a bigger script!
+#
+# Basescript - https://github.com/evertramos/basescript
+#
+#-----------------------------------------------------------------------
+
+#-----------------------------------------------------------------------
+# This function has one main objective:
+# 1. Check if the IPv4 is a valid address
+#
+# You must/might inform the parameters below:
+# 1. IP address that should be checked
+# 2. [optional] (default: ) n/a
+#
+#-----------------------------------------------------------------------
+
+ip_check_ipv4()
+{
+    local LOCAL_IP_ADDRESS
+
+    LOCAL_IP_ADDRESS=${1:-null}
+
+    # Check required
+    [[ $LOCAL_IP_ADDRESS == "" || $LOCAL_IP_ADDRESS == null ]] && echoerr "You must inform the required argument(s) to the function: '${FUNCNAME[0]}'"
+
+    if [[ $LOCAL_IP_ADDRESS =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+        IP_IPV4=true
+    else
+        IP_IPV4=false
+    fi
+}
---
+++ ip/ip_get_external_ipv6.sh
@@ -0,0 +1,35 @@
+#-----------------------------------------------------------------------
+#
+# Basescript function
+#
+# The basescript functions were designed to work as abstract function,
+# so it could be used in many different contexts executing specific job
+# always remembering Unix concept DOTADIW - "Do One Thing And Do It Well"
+#
+# Developed by
+#   Evert Ramos <evert.ramos@gmail.com>
+#
+# Copyright Evert Ramos
+#
+#-----------------------------------------------------------------------
+#
+# Be careful when editing this file, it is part of a bigger script!
+#
+# Basescript - https://github.com/evertramos/basescript
+#
+#-----------------------------------------------------------------------
+
+#-----------------------------------------------------------------------
+# This function has one main objective:
+# 1. Try to get the external IPv6 address of the server
+#
+# You must/might inform the parameters below:
+# 1. n/a
+# 2. [optional] (default: ) n/a
+#
+#-----------------------------------------------------------------------
+
+ip_get_external_ipv6()
+{
+    IP_EXTERNAL_IPV6=$(ip addr show $(ip link | awk -F: '$0 !~ "lo|vir|wl|^[^0-9]"{print $2;getline}' | head -n 1) | grep "inet6\b" | awk '{print $2}' | cut -d/ -f1)
+}
