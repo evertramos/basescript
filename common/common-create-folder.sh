c4f7e0634e6a346ad49556ccf1f7b1171cab88ce
Evert Ramos <evert.ramos@cgu.gov.br>
2021-05-13 18:30:03 -0300

    Create folder try as sudo if error and exit function option or return error message

--- common/common-create-folder.sh
+++ common/common-create-folder.sh
@@ -25,21 +25,28 @@
 #
 # You must/might inform the parameters below:
 # 1. Folder name that should be created
-# 2. [optional] (default: ) n/a
+# 2. [optional] (default: true) Stop script execution on error
 #
 #-----------------------------------------------------------------------
 
 common_create_folder()
 {
-    local LOCAL_FOLDER 
+    local LOCAL_FOLDER LOCAL_STOP_EXECUTION_ON_ERROR
 
     LOCAL_FOLDER=${1}
+    LOCAL_STOP_EXECUTION_ON_ERROR=${2:-true}
 
     [[ $LOCAL_FOLDER == "" ]] && echoerror "You must inform the required argument(s) to the function: '${FUNCNAME[0]}'"
 
     [[ "$DEBUG" == true ]] && echo "Creating folder '$LOCAL_FOLDER'"
 
-    mkdir -p $LOCAL_FOLDER
+    mkdir -p $LOCAL_FOLDER > /dev/null 2>&1
+
+    [[ ! -d "$LOCAL_FOLDER" ]] && sudo mkdir -p $LOCAL_FOLDER > /dev/null 2>&1
+
+    [[ ! -d "$LOCAL_FOLDER" ]] && [[ "$LOCAL_STOP_EXECUTION_ON_ERROR" == true ]] && echoerror "The folder '$LOCAL_FOLDER' could not be created."
+
+    [[ ! -d "$LOCAL_FOLDER" ]] && COMMON_CREATE_FOLDER_ERROR = true
 
     return 0
 }
