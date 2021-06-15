b8c119a6b9d2fb346ac5d88bbda8b3a9776f3361
Evert Ramos <evert.ramos@cgu.gov.br>
2021-06-15 07:23:27 -0300

    Add an extra layer of security on deleting folder to avoid system deletion without intention

--- system/system-safe-delete-folder.sh
+++ system/system-safe-delete-folder.sh
@@ -28,31 +28,38 @@
 # 2. [optional] (default: 'server') The path allowed by the script to
 # delete the requested folder, it can not be empty ('/')
 # 3. [optional] (default: false) Delete using 'sudo' option
+# 4. [optional] (default: false) Skip all security validation
 #
 #-----------------------------------------------------------------------
 
 # ----------------------------------------------------------------------
 # [IMPORTANT]
-# Use this function with caution! We recommend to always set the second
-# argument in order to keep restrict your script actions to a specific
-# path in your server and keep an eye on the SUDO option on deleting
+# Use this function with caution! We recommend yout to set the second
+# argument in order to restrict your script actions in a specific
+# folder path and keep an eye on the SUDO option on deleting
 # ----------------------------------------------------------------------
 system_safe_delete_folder()
 {
-    local LOCAL_SAFE_DELETE_FOLDER LOCAL_ALLOWED_DELETE_BASE_PATH LOCAL_BASE_PATH
+    local LOCAL_SAFE_DELETE_FOLDER LOCAL_ALLOWED_DELETE_BASE_PATH LOCAL_SAFE_DELETE_FOLDER_BASE_PATH
 
     LOCAL_SAFE_DELETE_FOLDER=${1%/}
     LOCAL_ALLOWED_DELETE_BASE_PATH=${2:-server}
-    DELETE_USING_SUDO=${3:-true}
+    DELETE_USING_SUDO=${3:-false}
     LOCAL_ALLOWED_DELETE_BASE_PATH=${LOCAL_ALLOWED_DELETE_BASE_PATH%/}
+    SKIP_ALL_VALIDATION=${4:-false}
 
     [[ $LOCAL_SAFE_DELETE_FOLDER == "" ]] && echoerror "You must inform a path as an argument to the function '${FUNCNAME[0]}'"
     [[ $LOCAL_ALLOWED_DELETE_BASE_PATH == "" ]] && echoerror "It seems you have tried to delete an unauthorized path. Please watch your fingers! \n path: '$LOCAL_SAFE_DELETE_FOLDER' - function: '${FUNCNAME[0]}'"
 
-    LOCAL_BASE_PATH=${LOCAL_SAFE_DELETE_FOLDER#/}
-    LOCAL_BASE_PATH=${LOCAL_BASE_PATH%%/*}
-    [[ $LOCAL_ALLOWED_DELETE_BASE_PATH != $LOCAL_BASE_PATH ]] && echoerror "You are not authorized to delete '$LOCAL_SAFE_DELETE_FOLDER'"
-    
+    # Safe check if root of deletion folder is '/' or '.' or '..'
+    # avoiding delete a system folders
+    if [[ "$SKIP_ALL_VALIDATION" != true ]]; then
+        LOCAL_SAFE_DELETE_FOLDER_BASE_PATH=${LOCAL_SAFE_DELETE_FOLDER#/}
+        LOCAL_SAFE_DELETE_FOLDER_BASE_PATH=${LOCAL_SAFE_DELETE_FOLDER_BASE_PATH%%/*}
+        [[ $LOCAL_SAFE_DELETE_FOLDER_BASE_PATH == *"."* ]] || [[ $LOCAL_SAFE_DELETE_FOLDER_BASE_PATH == *"/"* ]] && echoerror "Gotcha you! You action did not pass our safe deletion mode,\nthe path can not contain dots (.) or be null, which implies a system root folder."
+        [[ $LOCAL_ALLOWED_DELETE_BASE_PATH != "/$LOCAL_SAFE_DELETE_FOLDER_BASE_PATH" ]] && echoerror "You are not authorized to delete '$LOCAL_SAFE_DELETE_FOLDER'"
+    fi
+
     [[ "$DEBUG" == true ]] && echowarning "Deleting folder '$LOCAL_SAFE_DELETE_FOLDER'"
 
     if [[ "$DELETE_USING_SUDO" == true ]]; then
