92c55aff7acb813ffc343e237db1bd90cb066e89
Evert Ramos <evert.ramos@cgu.gov.br>
2021-08-02 09:33:39 -0300

    Add delete user and remove user ssh key from a container

---
+++ docker/docker-delete-user.sh
@@ -0,0 +1,52 @@
+#-----------------------------------------------------------------------
+#
+# Basescript function
+#
+# The basescript functions were designed to work as abstract function,
+# so it could be used in many different contexts executing specific job
+# always remembering Unix concept DOTADIW - "Do One Thing And Do It Well"
+#
+# Developed by
+#   Evert Ramos <evert.ramos@gmail.com>
+#
+# Copyright Evert Ramos
+#
+#-----------------------------------------------------------------------
+#
+# Be careful when editing this file, it is part of a bigger script!
+#
+# Basescript - https://github.com/evertramos/basescript
+#
+#-----------------------------------------------------------------------
+
+#-----------------------------------------------------------------------
+# This function has one main objective:
+# 1. Delete a user from a container
+#
+# You must/might inform the parameters below:
+# 1. Container name
+# 2. Username
+# 3. [optional] (default: )
+#
+#-----------------------------------------------------------------------
+
+docker_delete_user()
+{
+    local LOCAL_USER_NAME LOCAL_CONTAINER
+    
+    LOCAL_USER_NAME=${1:-null}
+    LOCAL_CONTAINER=${2:-null}
+
+    [[ $LOCAL_CONTAINER == "" || $LOCAL_CONTAINER == null ]] && echoerror "You must inform the required argument(s) to the function: '${FUNCNAME[0]}'"
+
+    [[ "$DEBUG" == true ]] && echo "Deleting user '$LOCAL_USER_NAME' from container '$LOCAL_CONTAINER'."
+
+    if [[ "$SILENT" == true ]]; then
+        docker exec -it $LOCAL_CONTAINER userdel --remove $LOCAL_USER_NAME 2>&1 > /dev/null
+    else
+        docker exec -it $LOCAL_CONTAINER userdel --remove $LOCAL_USER_NAME
+    fi
+
+    return 0
+}
+
---
+++ docker/docker-remove-user-key.sh
@@ -0,0 +1,49 @@
+#-----------------------------------------------------------------------
+#
+# Basescript function
+#
+# The basescript functions were designed to work as abstract function,
+# so it could be used in many different contexts executing specific job
+# always remembering Unix concept DOTADIW - "Do One Thing And Do It Well"
+#
+# Developed by
+#   Evert Ramos <evert.ramos@gmail.com>
+#
+# Copyright Evert Ramos
+#
+#-----------------------------------------------------------------------
+#
+# Be careful when editing this file, it is part of a bigger script!
+#
+# Basescript - https://github.com/evertramos/basescript
+#
+#-----------------------------------------------------------------------
+
+#-----------------------------------------------------------------------
+# This function has one main objective:
+# 1. Remove a user key from a container
+#
+# You must/might inform the parameters below:
+# 1. Container name
+# 2. Username
+# 3. [optional] (default: )
+#
+#-----------------------------------------------------------------------
+
+docker_remove_user_ssh_key()
+{
+    local LOCAL_CONTAINER LOCAL_USER_NAME
+
+    LOCAL_CONTAINER="${1:-null}"
+    LOCAL_USER_NAME="${2:-null}"
+       
+    [[ $LOCAL_CONTAINER == "" ]] && echoerror "You must inform a container to the function: '${FUNCNAME[0]}'"
+
+    [[ "$DEBUG" == true ]] && echo "Removing $LOCAL_USER_NAME's key in $LOCAL_CONTAINER"
+
+    if [[ "$SILENT" == true ]]; then
+        docker exec -it $LOCAL_CONTAINER bash -c "cd && sed -i '\| $LOCAL_USER_NAME@|d' .ssh/authorized_keys" 2>&1 > /dev/null
+    else
+        docker exec -it $LOCAL_CONTAINER bash -c "cd && sed -i '\| $LOCAL_USER_NAME@|d' .ssh/authorized_keys"
+    fi
+}
