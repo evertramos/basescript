5c7de6e76e07e52908dde415e1ab0622011a48b2
Evert Ramos <evert.ramos@cgu.gov.br>
2021-09-02 06:54:26 -0300

    #11 Check variable ALLOW_RUN_WITH_SUDO

--- env/env-create-if-not-exists.sh
+++ env/env-create-if-not-exists.sh
@@ -32,21 +32,25 @@
 
 env_create_if_not_exists()
 {
-    local LOCAL_FULL_PATH LOCAL_ENV_EXTENTION LOCAL_ENV_FILE_NAME LOCAL_ENV_EXAMPLE_FILE LOCAL_ENV_FINAL_FILE
+    local LOCAL_FULL_PATH LOCAL_ENV_EXTENTION LOCAL_ENV_FILE_NAME LOCAL_ENV_EXAMPLE_FILE LOCAL_ENV_FINAL_FILE LOCAL_ALLOW_RUN_WITH_SUDO
 
     LOCAL_FULL_PATH=${1:-null}
     LOCAL_ENV_EXTENTION=${2:-".example"}
     LOCAL_ENV_FILE_NAME=${3:-".env"}
     LOCAL_ENV_EXAMPLE_FILE="${LOCAL_FULL_PATH%/}/$LOCAL_ENV_FILE_NAME$LOCAL_ENV_EXTENTION"
     LOCAL_ENV_FINAL_FILE="${LOCAL_FULL_PATH%/}/$LOCAL_ENV_FILE_NAME"
+    LOCAL_ALLOW_RUN_WITH_SUDO=${ALLOW_RUN_WITH_SUDO:-true}
 
     [[ $LOCAL_FULL_PATH == "" || $LOCAL_FULL_PATH == null ]] && echoerror "You must inform the required argument(s) to the function: '${FUNCNAME[0]}'"
 
-    [[ "$DEBUG" == true ]] && echo "[env_create_if_not_exists] Checking if file '$LOCAL_ENV_FINAL_FILE' already exists."
+    [[ "$DEBUG" == true ]] && echoline "[env_create_if_not_exists] Checking if file '$LOCAL_ENV_FINAL_FILE' already exists."
     [[ -f "$LOCAL_ENV_FINAL_FILE" ]] && return 0
      
-    [[ "$DEBUG" == true ]] && echo "Creating file '$LOCAL_ENV_FINAL_FILE' from '$LOCAL_ENV_EXAMPLE_FILE'."
-    
-    cp $LOCAL_ENV_EXAMPLE_FILE $LOCAL_ENV_FINAL_FILE
-    [[ ! -f "$LOCAL_ENV_FINAL_FILE" ]] && sudo cp $LOCAL_ENV_EXAMPLE_FILE $LOCAL_ENV_FINAL_FILE
+    [[ "$DEBUG" == true ]] && echoline "Creating file '$LOCAL_ENV_FINAL_FILE' from '$LOCAL_ENV_EXAMPLE_FILE'."
+
+    # Allows 'sudo' to run this function if destination path it's not owned by the current user
+    [[ "$LOCAL_ALLOW_RUN_WITH_SUDO" == true ]] && ! system_check_user_folder_owner ${LOCAL_FULL_FILE_PATH%/*} && LOCAL_RUN_WITH_SUDO=sudo
+
+    $LOCAL_RUN_WITH_SUDO cp $LOCAL_ENV_EXAMPLE_FILE $LOCAL_ENV_FINAL_FILE
+    [[ ! -f "$LOCAL_ENV_FINAL_FILE" ]] && echoerror "Error creating file $LOCAL_ENV_FINAL_FILE from $LOCAL_ENV_FINAL_FILE - function: '${FUNCNAME[0]}'"
 }
