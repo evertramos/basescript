955e0a43a6a9d79c01902fa406655971f2bf0598
Evert Ramos <evert.ramos@gmail.com>
2023-05-14 17:14:50 -0300

    Remove check docker on init script

--- init/starts-initial-check.sh
+++ init/starts-initial-check.sh
@@ -36,9 +36,6 @@ starts_initial_check()
     LOCAL_PID_FILE=${1:-$PID_FILE}
     LOCAL_CHECK_BASE_ENV_FILE=${2:-false}
 
-    # Check if docker is installed
-    run_function check_docker
-
     if [[ "$LOCAL_CHECK_BASE_ENV_FILE" == true ]]; then
       # Check if there is an .env file in base folder
       run_function check_base_env_file
